263940b768ca3fac3d4ab0fbaec3ee6e93a359ad
Ncookie <ncukiert@gmail.com>
2022-09-28 18:53:01 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+
+	// Imports:
+	import Nav from '$lib/Nav.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import '../app.css';
+	
+</script>
+
+<!-- #################################################################################################### -->
+
+<!-- Navbar -->
+<Nav />
+
+<!-- App Content -->
+<main><slot /></main>
+
+<!-- Footer -->
+<Footer />
+
+<!-- #################################################################################################### -->
+
+<style>
+
+	main {
+		padding: 1em;
+	}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+
+	// Imports:
+	import { onMount } from 'svelte';
+
+	// Initializations & Exports:
+	// :3
+
+	onMount(() => {
+		// :3
+	});
+	
+</script>
+
+<!-- #################################################################################################### -->
+
+<!-- SvelteKit Dynamic Header -->
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Home Page" />
+</svelte:head>
+
+<!-- Page Content -->
+<section>
+	<span>Home Page</span>
+</section>
+
+<!-- #################################################################################################### -->
+
+<style>
+
+	/* CSS Goes Here */
+	
+</style>
