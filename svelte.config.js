63c2ba9534c1f47b08bef0346b79d2588e0fc456
Ncookiez <ncukiert@gmail.com>
2022-10-03 17:34:51 -0300

    Static -> IPFS Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import preprocess from 'svelte-preprocess';
-import staticAdapter from '@sveltejs/adapter-static';
+import ipfsAdapter from 'sveltejs-adapter-ipfs';
 import netlifyAdapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: process.env.BUILD_STATIC ? staticAdapter({ fallback: 'index.html' }) : netlifyAdapter()
+		adapter: process.env.BUILD_STATIC ? ipfsAdapter({ fallback: 'index.html' }) : netlifyAdapter()
 	}
 };
 
