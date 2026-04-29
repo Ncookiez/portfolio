b2962da34dcbc018cf6d43f1e83ae9b6f7dfaf82
Ncookiez <ncukiert@gmail.com>
2026-04-29 11:31:16 -0400

    removed netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import staticAdapter from '@sveltejs/adapter-static';
-import netlifyAdapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: process.env.BUILD_STATIC ? staticAdapter() : netlifyAdapter()
+		adapter: staticAdapter()
 	}
 };
 
